d60e07c26d0102a739e8a7ef534812924a21b11b
Abdullah Tanveer <abdullahch8860@gmail.com>
2023-12-23 16:17:52 +0500

    updating  jobs list

--- components/home/welcome/Welcome.jsx
+++ components/home/welcome/Welcome.jsx
@@ -1,15 +1,62 @@
-import React from 'react'
-import { View, Text } from 'react-native'
+import React,{useState}from 'react'
+import { View, Text ,TextInput, TouchableOpacity,Image,FlatList} from 'react-native';
+import { icons,SIZES } from '../../../constants';
+import {useRouter} from 'expo-router';
 
 import styles from './welcome.style'
 
+ 
+const jobTypes = ['Full Time','Part Time','Freelance','Internship']
 const Welcome = () => {
+  const router=useRouter();
+  const[activeJobType,setActiveJobType]=useState('Full Time');
   return (
     <View> 
      <View style={styles.container}>
        <Text style={styles.userName}>Hello Abdullah</Text>
        <Text style={styles.welcomeMessage}>Find your perfect job</Text>
+        
      </View>
+     <View style={ styles.searchContainer}>
+        <View style={styles.searchWrapper}>
+          <TextInput 
+          style={styles.searchInput}
+          placeholder="Search for job"
+          value=''
+          onChange={(text) => console.log(text)}
+
+          />
+
+        </View>
+        <TouchableOpacity style={styles.searchBtn}>
+          <Image
+          source={icons.search}
+          style={styles.searchBtnImage}
+          reziseMode="contain"
+          />
+        
+          </TouchableOpacity>
+       </View>
+       <View style={styles.tabsContainer}>
+        <FlatList
+        data={jobTypes}
+        renderItem={({item}) => (
+          <TouchableOpacity 
+          style={styles.tab(activeJobType,item)}
+          onPress={() => {setActiveJobType(item);
+            router.push(`/search/${item}`);
+          }}
+          >
+            <Text style={styles.tabText(activeJobType,item)}>{item}</Text>
+             
+          </TouchableOpacity>
+           
+        )}
+        keyExtractor={(item) => item}
+        contentContainerStyle={{columnGap: SIZES.small}}
+        horizontal
+        />
+       </View>
      </View>
   )
 }
--- components/home/welcome/welcome.style.js
+++ components/home/welcome/welcome.style.js
@@ -57,11 +57,12 @@ const styles = StyleSheet.create({
     marginTop: SIZES.medium,
   },
   tab: (activeJobType, item) => ({
-    paddingVertical: SIZES.small / 2,
+    paddingVertical: SIZES.small/2,
     paddingHorizontal: SIZES.small,
     borderRadius: SIZES.medium,
     borderWidth: 1,
     borderColor: activeJobType === item ? COLORS.secondary : COLORS.gray2,
+     
   }),
   tabText: (activeJobType, item) => ({
     fontFamily: FONT.medium,
