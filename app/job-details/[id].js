1e9ce46b10cf73dbf1f276ec8fd9d0397a441fea
Abdullah Tanveer <abdullahch8860@gmail.com>
2023-12-26 21:52:28 +0500

    updating job details screen

--- app/job-details/[id].js
+++ app/job-details/[id].js
@@ -1,34 +1,90 @@
-import React,{useState,useCallback} from 'react';
+import React, { useState, useCallback } from "react";
 import {
-    Text,
-    View,
-    SafeAreaView,
-    RefreshControl,
-    ScrollView,
+  Text,
+  View,
+  SafeAreaView,
+  RefreshControl,
+  ScrollView,
+  ActivityIndicator,
+} from "react-native";
+import { useRouter, Stack, useGlobalSearchParams } from "expo-router";
+import { COLORS, icons, SIZES } from "../../constants";
+import {
+  Company,
+  JobAbout,
+  JobFooter,
+  JobTabs,
+  ScreenHeaderBtn,
+  Screenheaderbtn,
+  Specifics,
+} from "../../components";
+import useFetch from "../../hooks/useFetch";
 
-} from 'react-native';
-import {useRouter,Stack,useGlobalSearchParams} from 'expo-router';
-import {COLORS,icons,SIZES} from '../../constants';
-import {Company,JobAbout,JobFooter,JobTabs,Screenheaderbtn,
-Specifics} from '../../components';
-import useFetch from '../../hooks/useFetch';
+const tabs = ["About", "Qualifications", "Resposnsibilities"];
 
+const JobDetails = () => {
+  const router = useRouter();
+  const params = useGlobalSearchParams();
+  const [activeTab, setActiveTab] = useState(tabs[0]);
 
-const  JobDetails = () => {
-    const router = useRouter();
-    const params=useGlobalSearchParams();
-
-    const[data,isloading,error,refetch]=useFetch( 'job-details',{
-        job_id:params.id,
-    });
+  const { data, isloading, error, refetch } = useFetch("job-details", {
+    job_id: params.id,
+  });
+  const [refreshing, setRefreshing] = useState(false);
+  const onRefresh = () => {};
   return (
-      <SafeAreaView style={{flex:1,backgroundColor:COLORS.lightWhite}}>
-  <Stack.Screen>
+    <SafeAreaView style={{ flex: 1, backgroundColor: COLORS.lightWhite }}>
+      <Stack.Screen
+        options={{
+          headerStyle: { backgroundColor: COLORS.lightWhite },
+          headerTitle: "",
+          headerShadowVisible: false,
+          headerBackVisible: false,
+          headerLeft: () => (
+            <ScreenHeaderBtn
+              iconUrl={icons.left}
+              handlePress={() => router.back()}
+              dimension="60%"
+            />
+          ),
+          headerRight: () => (
+            <ScreenHeaderBtn iconUrl={icons.share} dimension="60%" />
+          ),
+        }}
+      />
+      <>
+        <ScrollView
+          showsVerticalScrollIndicator={false}
+          refreshControl={
+            <RefreshControl refreshing={refreshing} onRefresh={onRefresh} />
+          }
+        >
+          {isloading ? (
+            <ActivityIndicator size="large" color={COLORS.primary} />
+          ) : error ? (
+            <Text>something went wrong</Text>
+          ) : data.length === 0 ? (
+            <Text>No data</Text>
+          ) : (
+            <View style={{ padding: SIZES.medium, paddingBottom: 100 }}>
+              <Company
+                companyLogo={data[0].employer_logo}
+                jobTitle={data[0].job_title}
+                companyName={data[0].employer_name}
+                Location={data[0].job_country}
+              />
 
-  </Stack.Screen>
+              <JobTabs
+                tabs={tabs}
+                activeTab={activeTab}
+                setActiveTab={setActiveTab}
+              />
+            </View>
+          )}
+        </ScrollView>
+      </>
+    </SafeAreaView>
+  );
+};
 
-      </SafeAreaView>
-  )
-}
-
-export default  JobDetails
+export default JobDetails;
